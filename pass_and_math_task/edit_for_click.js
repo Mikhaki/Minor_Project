166acd1f9f2ed38b77a432d231f1b1a77c448bcc
Mikhaki <mh4waretgirl@gmail.com>
2025-09-24 10:28:30 +0300

    edited pass pages files

--- pass_and_math_task/edit_for_click.js
+++ pass_and_math_task/edit_for_click.js
@@ -1,7 +1,15 @@
 function find_edit(){
-    const item  = document.getElementById('new')
-    console.log(item.innerText)
-    item.innerHTML="<b>оригинальный фрагмент изменен</b>"
+    const change_name_node  = document.getElementById('name')
+    console.log(change_name_node.innerText)
+    change_name_node.innerHTML="<b>Elena</b>"
+
+    const change_surname_node  = document.getElementById('surname')
+    console.log(change_surname_node.innerText)
+    change_surname_node.innerHTML="<b>Mikhailova</b>"
+
+    const change_date_node  = document.getElementById('date')
+    console.log(change_date_node.innerText)
+    change_date_node.innerHTML="<b>05.06.2004</b>"
 }
 
 const node_for_click = document.getElementById("for click")
